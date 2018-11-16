85a555b35c95673157f4c0ef4a71adee3e893f44
dbahlburg <dominik.bahlburg@alumni.ubc.ca>
2018-11-16 16:39:38 +0100

    finished first working version with t-test, anova and typeI and typeII errors

--- app.R
+++ app.R
@@ -6,48 +6,53 @@ library(shinydashboard)
 library(gridExtra)
 library(shinyWidgets)
 
-#approx_distr <- data_frame(approx_mean = numeric(), approx_sd = numeric())
-
+drawn_distr <- data_frame(approx_mean = numeric(), approx_sd = numeric())
 #Effekt von Stichprobengroesse auf Praezision der Verteilungsbeschreibung
+
+test_data <- read.csv('data/test_data.csv')
 
 ui <- dashboardPage(skin = 'blue',
                     
-                    dashboardHeader(title = 'Kritische Statistik'),
+                    
+                    dashboardHeader(title = 'Limited bayesian statistics',
+                                    titleWidth = '280'),
                     
                     dashboardSidebar(width = '240px',
                                      sidebarMenu(
                                        menuItem('Reconstructing distributions', tabName = 'precision',icon = icon('beer')),
-                                       menuItem('Sensitivity of p-values', tabName = 'p_values', icon = icon('beer'))
+                                       menuItem('Sample size vs. Type I error', tabName = 'type1', icon = icon('beer')),
+                                       menuItem('Sample size vs. Type II error', tabName = 'type2', icon = icon('beer'))
                                      )
                     ),
                     
                     dashboardBody(
                       tabItems(
                         tabItem(tabName = 'precision',
+                                withMathJax(),
                                 box(status = 'primary',
                                     title = 'Reconstructing a known distribution',
                                     width = 12,
                                     solidHeader = T,
                                     plotOutput('distribution'),
                                     chooseSliderSkin('Flat', color = '#5796c5'),
-                                    sliderInput('sample_size','Sample Size',min = 0, 
+                                    sliderInput('sample_size','Sample Size',min = 2, 
                                                 max = 100,value = 2, animate = T),
-                                    div(style = 'display:inline-block', textInputAddon(inputId = "mean_in", label = "Mean", value = 0, addon = NULL),
-                                        verbatimTextOutput(outputId = "mean_in")),
-                                    div(style = 'display:inline-block', textInputAddon(inputId = "sd_in", label = "Standard Deviation", value = 1, addon = NULL),
-                                        verbatimTextOutput(outputId = "sd_in"))
+                                    div(style = 'display:inline-block', numericInput('mean_in',label = '\\(\\bar{x}\\)', value = 0)),#$$\\bar{x}$$
+                                    div(style = 'display:inline-block', numericInput('sd_in',label = '\\(\\sigma\\)', value = 1, min = 0.2, step = 0.2)),
+                                    div(style = 'display:inline-block', numericInput('iteration_in',label = 'Number of Interations', value = 1, min = 1, step = 1))
                                 )
                         ),
-                        tabItem(tabName = 'p_values',
+                        tabItem(tabName = 'type1',
                                 box(status = 'primary',
-                                    title = 'How many samples do you need for a reliable p-value',
+                                    title = 'How likely is it to make a type I error?',
                                     width = 12,
                                     solidHeader = T,
                                     column(1,
                                            dropdown(pickerInput(
                                              label = 'select test',
                                              inputId = "selected_test", 
-                                             choices = c('ANOVA','pairwise t-test'), 
+                                             choices = c('ANOVA' = 'anova','pairwise t-test' = 't_test'),
+                                             selected = 'ANOVA',
                                              options = list(
                                                `actions-box` = F, 
                                                size = 10
@@ -61,9 +66,40 @@ ui <- dashboardPage(skin = 'blue',
                                            width = '200px')
                                     ),
                                     column(11,
-                                    plotOutput('p_plots'),
-                                    chooseSliderSkin('Flat', color = '#5796c5'),
-                                    sliderInput('psample_size', 'Sample Size', min = 2, max = 50, value = 2, animate = T))))
+                                           textOutput('test_name'),
+                                           plotOutput('type1_plot'),
+                                           chooseSliderSkin('Flat', color = '#5796c5'),
+                                           sliderInput('psample_size', 'Sample Size', min = 2, max = 50, value = 2, animate = T)))),
+                        tabItem(tabName = 'type2',
+                                box(status = 'primary',
+                                    title = 'How likely is it to make a type II error?',
+                                    width = 12,
+                                    solidHeader = T,
+                                    column(1,
+                                           dropdown(pickerInput(
+                                             label = 'select test',
+                                             inputId = "selected_test2",
+                                             choices = c('ANOVA' = 'anova','pairwise t-test' = 't_test'),
+                                             selected = 'ANOVA',
+                                             options = list(
+                                               `actions-box` = F,
+                                               size = 10
+                                             ),
+                                             multiple = F
+                                           ),
+                                           style = 'material-circle',
+                                           size = 'sm',
+                                           status = 'danger',
+                                           icon = icon("gear"),
+                                           width = '200px')
+                                    ),
+                                    column(11,
+                                           textOutput('test_name2'),
+                                           plotOutput('type2_plot'),
+                                           chooseSliderSkin('Flat', color = '#5796c5'),
+                                           sliderInput('psample_size2', 'Sample Size', min = 2, max = 50, value = 2, animate = T))))
+                        
+                       
                       )
                     )
 )
@@ -71,47 +107,84 @@ ui <- dashboardPage(skin = 'blue',
 server <- function(input, output) {
   
   approx_distr <- reactive({
-    samp <- rnorm(input$sample_size, mean = input$mean_in, sd = input$sd_in)
-    approx_distr <- data.frame(m = mean(samp), s = sd(samp))
-    approx_distr
-    #for (i in 1:10){
-    #  samps <- rnorm(input$sample_size, mean = input$mean_in, sd = input$sd_in)
-    #  mean_samps <- mean(samps)
-    #  sd_samps <- sd(samps)
-    #  approx_distr <- rbind(approx_distr, c(mean_samps, sd_samps))
-    #}
-    #approx_extremes <- data.frame(min_sd = min(approx_distr$approx_sd), max_sd = max(approx_distr$approx_sd), mean_mean = mean(approx_distr$approx_mean))
+    drawn_distr <- data_frame(variable = rep(1:input$iteration_in, each = input$sample_size),
+                              value = rnorm(input$iteration_in*input$sample_size, mean = input$mean_in, sd = input$sd_in))
+    drawn_distr %>%
+      group_by(variable) %>%
+      summarise(MEAN = mean(value),
+                SD = sd(value))
+
+})
+  
+  output$test_name <- renderPrint({
+    if (input$selected_test == 'anova'){
+    cat('ANOVA')}
+    else if (input$selected_test == 't_test'){
+      cat('Pairwise t-test')
+    }
   })
   
+  output$test_name2 <- renderPrint({
+    if (input$selected_test2 == 'anova'){
+      cat('ANOVA')}
+    else if (input$selected_test2 == 't_test'){
+      cat('Pairwise t-test')
+    }
+  })
   
+  selected_type1 <- reactive({
+    test_data %>%
+      filter(diff == 'no') %>%
+      filter(test == input$selected_test & samples == input$psample_size)
+  })
+  
+  selected_type2 <- reactive({
+    test_data %>%
+      filter(diff == 'yes') %>%
+      filter(test == input$selected_test2 & samples == input$psample_size2)
+  })
   
   output$distribution <- renderPlot({
-    ggplot(data.frame(x_lim = c(as.numeric(input$mean_in) - 3*as.numeric(input$sd_in), as.numeric(input$mean_in) + 3*as.numeric(input$sd_in))), aes(x_lim)) +
-      stat_function(fun = dnorm, n = 300, args = list(mean = as.numeric(input$mean_in), sd = as.numeric(input$sd_in))) +
-      stat_function(fun = dnorm, n = 300, args = list(mean = approx_distr()$m, sd = approx_distr()$s)) +
-      labs(y = '', x = '') +
-      scale_y_continuous(limits = c(0,0.5)) +
-      theme_classic()
+    
+    purrr::reduce2(.x = approx_distr()$MEAN,
+                   .y = approx_distr()$SD,
+                   .init = ggplot(data.frame(x_lim = c(as.numeric(input$mean_in) - 5*as.numeric(input$sd_in), as.numeric(input$mean_in) + 5*as.numeric(input$sd_in))), aes(x_lim))+
+                     stat_function(fun = dnorm, n = 300, args = list(mean = input$mean_in, sd = input$sd_in)) +
+                     labs(y = '', x = '') +
+                     scale_y_continuous(limits = c(0,1)) +
+                     theme_bw()+
+                     theme(panel.grid.minor = element_blank(),
+                           panel.grid.major.x = element_blank()),
+                   function(prev, .x, .y) {
+                     prev + stat_function(fun = dnorm,
+                                          n = 300,
+                                          args = list(mean = .x,
+                                                      sd = .y),
+                                          alpha = 0.4, linetype = '22', colour = '#dd3e54')
+                   })
   })
   
+  output$type1_plot <- renderPlot({
+  ggplot(selected_type1(),aes(x = diff_mean, y = sd, fill = 1-correctness)) +
+    geom_raster() +
+    scale_fill_gradient(low = '#6be585', high = '#dd3e54', limits = c(0,0.3), breaks = c(0,0.3),
+                        guide = guide_colorbar(title = '', ticks = FALSE,
+                                               nbin = 100)) +
+    labs(x = expression(Delta~bar(x)),y = expression(sigma))+
+    theme_bw() +
+    theme(panel.grid = element_blank())
+  })
+  
+  output$type2_plot <- renderPlot({
+    ggplot(selected_type2(),aes(x = diff_mean, y = sd, fill = 1-correctness)) +
+      geom_raster() +
+      scale_fill_gradient(low = '#6be585', high = '#dd3e54', limits = c(0,1), breaks = c(0,0.5,1),
+                          guide = guide_colorbar(title = '', ticks = FALSE,
+                                                 nbin = 100)) +
+      labs(x = expression(Delta~bar(x)),y = expression(sigma))+
+      theme_bw() +
+      theme(panel.grid = element_blank())
+  })
 }
 shinyApp(ui = ui, server = server)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- create_data.R
+++ create_data.R
@@ -2,49 +2,41 @@
 library(tidyverse)
 library(broom)
 
-t_sim <- data_frame(samples = numeric(),
-                    s_mean = numeric(),
-                    s_sd = numeric(),
-                    p_value = numeric())
-p_values <- numeric()
-selected_test <- "test_anova"
+t_sim2 <- data_frame(samples = numeric(),
+                     s_mean = numeric(),
+                     s_sd = numeric(),
+                     p_value = numeric())
+p_values2 <- numeric()
+
 
 for (i in 2:50){
   for (k in seq(0.5,10,0.5)){
     for (m in seq(0.5,10,0.5)){
       for (l in 1:50){
         
-        #switch(selected_test,
-        #       
-        #       ttest = {gen_data <- data_frame(variable1 = rnorm(i, mean = 0, sd = 1),
-        #                                       variable2 = rnorm(i, mean = 0+k, sd = 1+m))
-        #       t_test <- t.test(gen_data$variable1, gen_data$variable2, paired = T)
-        #       p_values <- rbind(p_values, t_test$p.value)
-        #                },
-        #       test_anova = {
-        gen_data <- data_frame(variable = rep(c('a','b','c'), each = i),
-                                                    value = c(rnorm(i, mean = 0, sd = 1),rnorm(i, mean = 0+k, sd = 1+m),
-                                                              rnorm(i, mean = 0+2*k, sd = 1+2*m)))
-               anova_results <- tidy(aov(value~variable, data = gen_data))
-               p_values <- rbind(p_values, anova_results$p.value[1])
-                 
-               #}
-               #)
+        gen_data <- data_frame(variable1 = rnorm(i, mean = 0, sd = m),
+                               variable2 = rnorm(i, mean = k, sd = m))
+        t_test <- t.test(gen_data$variable1, gen_data$variable2, paired = T)
+        p_values2 <- rbind(p_values2, t_test$p.value)
         
         
+        # gen_data <- data_frame(variable = rep(c('a','b','c'), each = i),
+        #                        value = c(rnorm(i, mean = 0, sd = m),rnorm(i, mean = 0, sd = m),
+        #                                  rnorm(i, mean = 0, sd = m)))
+        # anova_results <- tidy(aov(value~variable, data = gen_data))
+        # p_values <- rbind(p_values, anova_results$p.value[1])
         
-        
+        #)
       }
-      right_decision <- sum(p_values < 0.05)/length(p_values)
-      t_sim <- rbind(t_sim, c(i,k,m,right_decision))
+      right_decision <- sum(p_values2 < 0.05)/length(p_values2)
+      t_sim2 <- rbind(t_sim2, c(i,k,m,right_decision))
       
-      p_values <- numeric()
+      p_values2 <- numeric()
     }  
   }
 }
 
-write.csv(t_sim, file = 'data/anova_data.csv')
-
+write.csv(t_sim3, file = 'diff_ttest_data.csv')
 t_sim %>%
   rename_at(vars(colnames(t_sim)), ~ c('samples','diff_mean','diff_sd','correctness')) %>%
   filter(samples == 50) %>%
@@ -53,20 +45,29 @@ t_sim %>%
   scale_fill_gradient(high = '#6be585', low = '#dd3e54', limits = c(0,1), breaks = c(0,0.5,1),
                       guide = guide_colorbar(title = '', ticks = FALSE,
                                              nbin = 100)) +
-  labs(x = 'difference between means',y = 'difference between standard deviations') +
+  labs(x = expression(Delta~bar(x)),y = expression(Delta~sigma))+
   theme_bw() +
   theme(panel.grid = element_blank())
 
 
+t_nodiff_data <- read.csv('data/nodiff_ttest_data.csv')
+t_diff_data <- read.csv('data/diff_ttest_data.csv')
+aov_nodiff_data <- read.csv('data/nodiff_anova_data.csv')
+aov_diff_data <- read.csv('data/diff_anova_data.csv')
+
+aov_diff_data <- rename_at(aov_diff_data, vars(colnames(aov_diff_data)), ~ c('X','samples','diff_mean','sd','correctness'))
+
+aov_data <- rename_at(aov_data, vars(colnames(aov_data)), ~ c('X','samples','diff_mean','sd','correctness'))
+t_nodiff_data$test <- 't_test'
+t_diff_data$test <- 't_test'
+t_nodiff_data$diff <- 'no'
+t_diff_data$diff <- 'yes'
+
+aov_diff_data$test <- 'anova'
+aov_nodiff_data$test <- 'anova'
+aov_diff_data$diff <- 'yes'
+aov_nodiff_data$diff <- 'no'
 
 
-gen_data <- data_frame(variable = rep(c('a','b','c'), each = 3),
-                       value = c(rnorm(3, mean = 0, sd = 1),rnorm(3, mean = 0+1, sd = 1+1),rnorm(3, mean = 0+2*1, sd = 1+2*1)))
-a <- tidy(aov(value~variable, data = gen_data))
-
-
-
-
-
-
-
+test_data <- rbind(t_nodiff_data,t_diff_data,aov_nodiff_data,aov_diff_data)
+write.csv(test_data, file = 'data/test_data.csv')
