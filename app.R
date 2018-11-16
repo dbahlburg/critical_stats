d5a09cf644efd5d172acc0fa54bc4b0fb03d29af
dbahlburg <dominik.bahlburg@alumni.ubc.ca>
2018-11-16 11:55:25 +0100

    add generated data

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(shinydashboard)
 library(gridExtra)
 library(shinyWidgets)
 
+#approx_distr <- data_frame(approx_mean = numeric(), approx_sd = numeric())
 
 #Effekt von Stichprobengroesse auf Praezision der Verteilungsbeschreibung
 
@@ -13,44 +14,86 @@ ui <- dashboardPage(skin = 'blue',
                     
                     dashboardHeader(title = 'Kritische Statistik'),
                     
-                    dashboardSidebar(width = '190px',
+                    dashboardSidebar(width = '240px',
                                      sidebarMenu(
-                                       menuItem('Reconstruction', tabName = 'precision',icon = icon('beer')),
-                                       menuItem('Experimental Data', tabName = 'experiment', icon = icon('flask'))
+                                       menuItem('Reconstructing distributions', tabName = 'precision',icon = icon('beer')),
+                                       menuItem('Sensitivity of p-values', tabName = 'p_values', icon = icon('beer'))
                                      )
                     ),
                     
                     dashboardBody(
                       tabItems(
                         tabItem(tabName = 'precision',
-                                  box(status = 'primary',
-                                      title = 'Reconstructing a known distribution',
-                                      width = 12,
-                                      solidHeader = T,
-                                      plotOutput('distribution'),
-                                      chooseSliderSkin('Flat', color = '#5796c5'),
-                                      sliderInput('sample_size','Sample Size',min = 0, 
-                                                  max = 100,value = 2, animate = T)
-                                  )
-                        )
+                                box(status = 'primary',
+                                    title = 'Reconstructing a known distribution',
+                                    width = 12,
+                                    solidHeader = T,
+                                    plotOutput('distribution'),
+                                    chooseSliderSkin('Flat', color = '#5796c5'),
+                                    sliderInput('sample_size','Sample Size',min = 0, 
+                                                max = 100,value = 2, animate = T),
+                                    div(style = 'display:inline-block', textInputAddon(inputId = "mean_in", label = "Mean", value = 0, addon = NULL),
+                                        verbatimTextOutput(outputId = "mean_in")),
+                                    div(style = 'display:inline-block', textInputAddon(inputId = "sd_in", label = "Standard Deviation", value = 1, addon = NULL),
+                                        verbatimTextOutput(outputId = "sd_in"))
+                                )
+                        ),
+                        tabItem(tabName = 'p_values',
+                                box(status = 'primary',
+                                    title = 'How many samples do you need for a reliable p-value',
+                                    width = 12,
+                                    solidHeader = T,
+                                    column(1,
+                                           dropdown(pickerInput(
+                                             label = 'select test',
+                                             inputId = "selected_test", 
+                                             choices = c('ANOVA','pairwise t-test'), 
+                                             options = list(
+                                               `actions-box` = F, 
+                                               size = 10
+                                             ), 
+                                             multiple = F
+                                           ),
+                                           style = 'material-circle',
+                                           size = 'sm',
+                                           status = 'danger',
+                                           icon = icon("gear"),
+                                           width = '200px')
+                                    ),
+                                    column(11,
+                                    plotOutput('p_plots'),
+                                    chooseSliderSkin('Flat', color = '#5796c5'),
+                                    sliderInput('psample_size', 'Sample Size', min = 2, max = 50, value = 2, animate = T))))
                       )
                     )
 )
 
 server <- function(input, output) {
   
-  selected_experiment <- reactive({
-    flow_summary %>%
-      filter(origin == 'experiment') %>%
-      filter(stat_or_exp_no == input$exp_no)
+  approx_distr <- reactive({
+    samp <- rnorm(input$sample_size, mean = input$mean_in, sd = input$sd_in)
+    approx_distr <- data.frame(m = mean(samp), s = sd(samp))
+    approx_distr
+    #for (i in 1:10){
+    #  samps <- rnorm(input$sample_size, mean = input$mean_in, sd = input$sd_in)
+    #  mean_samps <- mean(samps)
+    #  sd_samps <- sd(samps)
+    #  approx_distr <- rbind(approx_distr, c(mean_samps, sd_samps))
+    #}
+    #approx_extremes <- data.frame(min_sd = min(approx_distr$approx_sd), max_sd = max(approx_distr$approx_sd), mean_mean = mean(approx_distr$approx_mean))
   })
- 
-  output$plots <- renderPlot({
-    p2 <- flow_plot(input_data = selected_station(), field_or_exp = 'field', bac_or_phyto = bac_vec,log_scale = input$logswitch2)
-    p <- grid.arrange(p1,p2, ncol=1)
-    print(p)
+  
+  
+  
+  output$distribution <- renderPlot({
+    ggplot(data.frame(x_lim = c(as.numeric(input$mean_in) - 3*as.numeric(input$sd_in), as.numeric(input$mean_in) + 3*as.numeric(input$sd_in))), aes(x_lim)) +
+      stat_function(fun = dnorm, n = 300, args = list(mean = as.numeric(input$mean_in), sd = as.numeric(input$sd_in))) +
+      stat_function(fun = dnorm, n = 300, args = list(mean = approx_distr()$m, sd = approx_distr()$s)) +
+      labs(y = '', x = '') +
+      scale_y_continuous(limits = c(0,0.5)) +
+      theme_classic()
   })
-
+  
 }
 shinyApp(ui = ui, server = server)
 
--- create_data.R
+++ create_data.R
@@ -1,5 +1,68 @@
+#create data for known distributions
+library(tidyverse)
+library(broom)
+
+t_sim <- data_frame(samples = numeric(),
+                    s_mean = numeric(),
+                    s_sd = numeric(),
+                    p_value = numeric())
+p_values <- numeric()
+selected_test <- "test_anova"
+
+for (i in 2:50){
+  for (k in seq(0.5,10,0.5)){
+    for (m in seq(0.5,10,0.5)){
+      for (l in 1:50){
+        
+        #switch(selected_test,
+        #       
+        #       ttest = {gen_data <- data_frame(variable1 = rnorm(i, mean = 0, sd = 1),
+        #                                       variable2 = rnorm(i, mean = 0+k, sd = 1+m))
+        #       t_test <- t.test(gen_data$variable1, gen_data$variable2, paired = T)
+        #       p_values <- rbind(p_values, t_test$p.value)
+        #                },
+        #       test_anova = {
+        gen_data <- data_frame(variable = rep(c('a','b','c'), each = i),
+                                                    value = c(rnorm(i, mean = 0, sd = 1),rnorm(i, mean = 0+k, sd = 1+m),
+                                                              rnorm(i, mean = 0+2*k, sd = 1+2*m)))
+               anova_results <- tidy(aov(value~variable, data = gen_data))
+               p_values <- rbind(p_values, anova_results$p.value[1])
+                 
+               #}
+               #)
+        
+        
+        
+        
+      }
+      right_decision <- sum(p_values < 0.05)/length(p_values)
+      t_sim <- rbind(t_sim, c(i,k,m,right_decision))
+      
+      p_values <- numeric()
+    }  
+  }
+}
+
+write.csv(t_sim, file = 'data/anova_data.csv')
+
+t_sim %>%
+  rename_at(vars(colnames(t_sim)), ~ c('samples','diff_mean','diff_sd','correctness')) %>%
+  filter(samples == 50) %>%
+  ggplot(.,aes(x = diff_mean, y = diff_sd, fill = correctness)) +
+  geom_raster() +
+  scale_fill_gradient(high = '#6be585', low = '#dd3e54', limits = c(0,1), breaks = c(0,0.5,1),
+                      guide = guide_colorbar(title = '', ticks = FALSE,
+                                             nbin = 100)) +
+  labs(x = 'difference between means',y = 'difference between standard deviations') +
+  theme_bw() +
+  theme(panel.grid = element_blank())
 
 
+
+
+gen_data <- data_frame(variable = rep(c('a','b','c'), each = 3),
+                       value = c(rnorm(3, mean = 0, sd = 1),rnorm(3, mean = 0+1, sd = 1+1),rnorm(3, mean = 0+2*1, sd = 1+2*1)))
+a <- tidy(aov(value~variable, data = gen_data))
 
 
 
